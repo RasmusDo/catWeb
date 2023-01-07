673aa5cb0327c06f78feed4a334798cd7846c454
ploxan <Rasmus.dufva@outlook.com>
2023-01-07 10:06:06 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import Navbar from './components/navbar';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Random from './pages/Random';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="component">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/random" element={<Random />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
