dbc433d95ede39b56bbbd016fbc20c39e18e3dda
ploxan <Rasmus.dufva@outlook.com>
2023-01-08 10:16:44 +0100

    some general changes to styling and Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,56 @@
+import { useState, useEffect, useCallback } from 'react';
+import Particles, { ISourceOptions, Main } from 'react-tsparticles';
+import { loadSeaAnemonePreset } from 'tsparticles-preset-sea-anemone';
+import './Home.css';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  function particlesInit(tsParticles) {
+    console.log('init', tsParticles);
+
+    loadSeaAnemonePreset(tsParticles);
+  }
+
+  const [cat, setCat] = useState([]);
+  const [quote, setQuote] = useState([]);
+
+  useEffect(() => {
+    quotes();
+    cats();
+  }, []);
+
+  const cats = async () => {
+    const response = await fetch('https://cataas.com/cat');
+
+    setCat(await response.json());
+  };
+
+  const quotes = async () => {
+    const response = await fetch('https://cat-fact.herokuapp.com/facts');
+
+    setQuote(await response.json());
+  };
+
+  return (
+    <div className="main">
+      <Particles
+        id="particleContainer"
+        options={{
+          fullScreen: { enable: false },
+          preset: 'seaAnemone',
+          container: '',
+        }}
+        init={particlesInit}
+      />
+      <h1>Here is the quote of the day!</h1>
+      <img
+        src="https://cataas.com/cat?width=500&height=500"
+        alt="randomcat"
+      ></img>
+      <ul className="quouteList">
+        {quote.map((data) => {
+          return <li key={data.user}>{data.text}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
